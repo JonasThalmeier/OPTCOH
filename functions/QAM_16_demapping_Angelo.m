acbf4cac4c59f329c1bea6e2ac462ec557f7aaa4
AngeloRollo00 <angelo.rollo15@gmail.com>
2024-06-04 17:33:59 +0200

    demapping function for BPS

---
+++ functions/QAM_16_demapping_Angelo.m
@@ -0,0 +1,208 @@
+function [demappedBits_Xpol,demappedSymb_Xpol,demappedConfig_Xpol] = QAM_16_demapping(downsampledSig_Xpol)
+% downsampledSig_Xpol = downsampledSig_Xpol(1:2:end); %downsample to 1SpS
+% downsampledSig_Ypol = downsampledSig_Ypol(1:2:end);
+division = 2; %round((abs(max(real(downsampledSig_Xpol)))+abs(min(real(downsampledSig_Xpol))))/2,2); 
+
+% Symbol Demapping
+demappedBits_Xpol = zeros(length(downsampledSig_Xpol),4); % Adjust size accordingly for bit pairs, etc.
+demappedSymb_Xpol = zeros(length(downsampledSig_Xpol),1);
+% demappedBits_Ypol = zeros(length(downsampledSig_Ypol),4); % Adjust size accordingly for bit pairs, etc.
+% demappedSymb_Ypol = zeros(length(downsampledSig_Ypol),1);
+demappedConfig_Xpol = zeros(length(downsampledSig_Xpol),1);
+%demappedConfig_Ypol = zeros(length(downsampledSig_Ypol),1);
+
+% Assuming QPSK and not accounting for noise, just a direct mapping
+for i = 1:length(downsampledSig_Xpol)
+    % This is a simplistic approach; real demapping would consider noise, etc.
+    if real(downsampledSig_Xpol(i)) > 0
+        if imag(downsampledSig_Xpol(i)) > 0
+            if real(downsampledSig_Xpol(i)) > division
+                if imag(downsampledSig_Xpol(i)) > division
+                    demappedBits_Xpol(i, :) = [1 0 0 0];
+                    demappedSymb_Xpol(i) = 8;
+                    demappedConfig_Xpol(i,:) = 3+1i*3;
+                else
+                    demappedBits_Xpol(i, :) = [1 0 0 1];
+                    demappedSymb_Xpol(i) = 9;
+                    demappedConfig_Xpol(i,:) = 3+1i*1;
+                end
+            else
+                if imag(downsampledSig_Xpol(i)) > division
+                    demappedBits_Xpol(i, :) = [1 1 0 0];
+                    demappedSymb_Xpol(i) = 12;
+                    demappedConfig_Xpol(i,:) = 1+1i*3;
+                else
+                    demappedBits_Xpol(i, :) = [1 1 0 1];
+                    demappedSymb_Xpol(i) = 13;
+                    demappedConfig_Xpol(i,:) = 1+1i*1;
+                end
+            end
+        else
+            if real(downsampledSig_Xpol(i)) > division
+                if imag(downsampledSig_Xpol(i)) > -division
+                    demappedBits_Xpol(i, :) = [1 0 1 1];
+                    demappedSymb_Xpol(i) = 11;
+                    demappedConfig_Xpol(i,:) = 3-1i*1;
+                else
+                    demappedBits_Xpol(i, :) = [1 0 1 0];
+                    demappedSymb_Xpol(i) = 10;
+                    demappedConfig_Xpol(i,:) = 3-1i*3;
+                end
+            else
+                if imag(downsampledSig_Xpol(i)) > -division
+                    demappedBits_Xpol(i, :) = [1 1 1 1];
+                    demappedSymb_Xpol(i) = 15;
+                    demappedConfig_Xpol(i,:) = 1-1i;
+                else
+                    demappedBits_Xpol(i, :) = [1 1 1 0];
+                    demappedSymb_Xpol(i) = 14;
+                    demappedConfig_Xpol(i,:) = 1-1i*3;
+                end
+            end
+        end
+    else
+         if imag(downsampledSig_Xpol(i)) > 0
+            if real(downsampledSig_Xpol(i)) > -division
+                if imag(downsampledSig_Xpol(i)) > division
+                    demappedBits_Xpol(i, :) = [0 1 0 0];
+                    demappedSymb_Xpol(i) = 4;
+                    demappedConfig_Xpol(i,:) = -1+1i*3;
+                else
+                    demappedBits_Xpol(i, :) = [0 1 0 1];
+                    demappedSymb_Xpol(i) = 5;
+                    demappedConfig_Xpol(i,:) = -1+1i*1;
+                end
+            else
+                if imag(downsampledSig_Xpol(i)) > division
+                    demappedBits_Xpol(i, :) = [0 0 0 0];
+                    demappedSymb_Xpol(i) = 0;
+                    demappedConfig_Xpol(i,:) = -3+1i*3;
+                else
+                    demappedBits_Xpol(i, :) = [0 0 0 1];
+                    demappedSymb_Xpol(i) = 1;
+                    demappedConfig_Xpol(i,:) = -3+1i*1;
+                end
+            end
+        else
+            if real(downsampledSig_Xpol(i)) > -division
+                if imag(downsampledSig_Xpol(i)) > -division
+                    demappedBits_Xpol(i, :) = [0 1 1 1];
+                    demappedSymb_Xpol(i) = 7;
+                    demappedConfig_Xpol(i,:) = -1-1i*1;
+                else
+                    demappedBits_Xpol(i, :) = [0 1 1 0];
+                    demappedSymb_Xpol(i) = 6;
+                    demappedConfig_Xpol(i,:) = -1-1i*3;
+                end
+            else
+                if imag(downsampledSig_Xpol(i)) > -division
+                    demappedBits_Xpol(i, :) = [0 0 1 1];
+                    demappedSymb_Xpol(i) = 3;
+                    demappedConfig_Xpol(i,:) = -3-1i*1;
+                else
+                    demappedBits_Xpol(i, :) = [0 0 1 0];
+                    demappedSymb_Xpol(i) = 2;
+                    demappedConfig_Xpol(i,:) = -3-1i*3;
+                end
+            end
+         end 
+    end
+end
+% 
+% for i = 1:length(downsampledSig_Ypol)
+%     This is a simplistic approach; real demapping would consider noise, etc.
+%     if real(downsampledSig_Ypol(i)) > 0
+%         if imag(downsampledSig_Ypol(i)) > 0
+%             if real(downsampledSig_Ypol(i)) > division
+%                 if imag(downsampledSig_Ypol(i)) > division
+%                     demappedBits_Ypol(i, :) = [1 0 0 0];
+%                     demappedSymb_Ypol(i) = 8;
+%                     demappedConfig_Ypol(i) = 3+i*3;
+%                 else
+%                     demappedBits_Ypol(i, :) = [1 0 0 1];
+%                     demappedSymb_Ypol(i) = 9;
+%                     demappedConfig_Ypol(i) = 3+i*1;
+%                 end
+%             else
+%                 if imag(downsampledSig_Ypol(i)) > division
+%                     demappedBits_Ypol(i, :) = [1 1 0 0];
+%                     demappedSymb_Ypol(i) = 12;
+%                     demappedConfig_Ypol(i) = 1+i*3;
+%                 else
+%                     demappedBits_Ypol(i, :) = [1 1 0 1];
+%                     demappedSymb_Ypol(i) = 13;
+%                     demappedConfig_Ypol(i) = 1+i*1;
+%                 end
+%             end
+%         else
+%             if real(downsampledSig_Ypol(i)) > division
+%                 if imag(downsampledSig_Ypol(i)) > -division
+%                     demappedBits_Ypol(i, :) = [1 0 1 1];
+%                     demappedSymb_Ypol(i) = 11;
+%                     demappedConfig_Ypol(i) = 3-i*1;
+%                 else
+%                     demappedBits_Ypol(i, :) = [1 0 1 0];
+%                     demappedSymb_Ypol(i) = 10;
+%                     demappedConfig_Ypol(i) = 3-i*3;
+%                 end
+%             else
+%                 if imag(downsampledSig_Ypol(i)) > -division
+%                     demappedBits_Ypol(i, :) = [1 1 1 1];
+%                     demappedSymb_Ypol(i) = 15;
+%                     demappedConfig_Ypol(i) = 1-i*1;
+%                 else
+%                     demappedBits_Ypol(i, :) = [1 1 1 0];
+%                     demappedSymb_Ypol(i) = 14;
+%                     demappedConfig_Ypol(i) = 1-i*3;
+%                 end
+%             end
+%         end
+%     else
+%         if imag(downsampledSig_Ypol(i)) > 0
+%             if real(downsampledSig_Ypol(i)) > -division
+%                 if imag(downsampledSig_Ypol(i)) > division
+%                     demappedBits_Ypol(i, :) = [0 1 0 0];
+%                     demappedSymb_Ypol(i) = 4;
+%                     demappedConfig_Ypol(i) = -1+i*3;
+%                 else
+%                     demappedBits_Ypol(i, :) = [0 1 0 1];
+%                     demappedSymb_Ypol(i) = 5;
+%                     demappedConfig_Ypol(i) = -1+i*1;
+%                 end
+%             else
+%                 if imag(downsampledSig_Ypol(i)) > division
+%                     demappedBits_Ypol(i, :) = [0 0 0 0];
+%                     demappedSymb_Ypol(i) = 0;
+%                     demappedConfig_Ypol(i) = -3+i*3;
+%                 else
+%                     demappedBits_Ypol(i, :) = [0 0 0 1];
+%                     demappedSymb_Ypol(i) = 1;
+%                     demappedConfig_Ypol(i) = -3+i*1;
+%                 end
+%             end
+%         else
+%             if real(downsampledSig_Ypol(i)) > -division
+%                 if imag(downsampledSig_Ypol(i)) > -division
+%                     demappedBits_Ypol(i, :) = [0 1 1 1];
+%                     demappedSymb_Ypol(i) = 7;
+%                     demappedConfig_Ypol(i) = -1-i*1;
+%                 else
+%                     demappedBits_Ypol(i, :) = [0 1 1 0];
+%                     demappedSymb_Ypol(i) = 6;
+%                     demappedConfig_Ypol(i) = -1-i*3;
+%                 end
+%             else
+%                 if imag(downsampledSig_Ypol(i)) > -division
+%                     demappedBits_Ypol(i, :) = [0 0 1 1];
+%                     demappedSymb_Ypol(i) = 3;
+%                     demappedConfig_Ypol(i) = -3-i*1;
+%                 else
+%                     demappedBits_Ypol(i, :) = [0 0 1 0];
+%                     demappedSymb_Ypol(i) = 2;
+%                     demappedConfig_Ypol(i) = -3-i*3;
+%                 end
+%             end
+%         end
+%     end
+% end
+end
