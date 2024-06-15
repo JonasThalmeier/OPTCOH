75a4ed7e15cfc7eb06a4611aaaa11e01e534d129
JonasThalmeier <s324419@studenti.polito.it>
2024-06-15 21:41:36 +0200

    SNR penalty function added

--- Delta_SNR.m
+++ Delta_SNR.m
@@ -12,24 +12,30 @@ load(strcat('TXsequences/TXsequence_', MODULATIONS(r) , '_64GBaud.mat'));
 % Parameters
 SpS_down = 4;
 SpS_up = SIG.Sps/SpS_down;
-OSNR_dB = 6:4:18;
 seq_lenght = length(SIG.Xpol.txSymb);
 B = 30;
 N = 50;
-sweep_par = 1:3;
+sweep_par = linspace(50e3,1e6,20);
+BER_goal = 1e-3;
 
 
 if r == 1
     M = 4;
     power_norm = 2;
+    SNR_opt = 2*log10(10*erfinv(1-2*BER_goal)^2);
 else
     M = 16;
     power_norm = 10;
+    SNR_opt = 10*log10(10*erfinv(1-8/3*BER_goal)^2);
 end
 
-BERmat = zeros(length(sweep_par), length(OSNR_dB));
+
+SNR = SNR_opt-.5;
+BER = ones(length(sweep_par),4);
+delta_SNR = zeros(length(sweep_par),1);
 for idx_sweep = 1:length(sweep_par)
-    for idx_OSNR = 1:length(OSNR_dB)
+    SNR = SNR_opt-.5;
+    while BER(idx_sweep,3)>BER_goal
         TX_BITS_Xpol = repmat(SIG.Xpol.bits,10,1); %repeat the bits 10 times to simulate the original transmission
         TX_BITS_Ypol = repmat(SIG.Ypol.bits,10,1); %repeat the bits 10 times to simulate the original transmission
 
@@ -43,15 +49,10 @@ for idx_sweep = 1:length(sweep_par)
         % Adding chromatic dispersion
         [X_CD,Y_CD]=Chromatic_Dispersion(X_distorted, Y_distorted, SIG.Sps, 1);
 
-        X_Ber_Tot_CMA = zeros(1,length(OSNR_dB));
-        Y_Ber_Tot_CMA = zeros(1,length(OSNR_dB));
-        X_Ber_Tot_LMS = zeros(1,length(OSNR_dB));
-        Y_Ber_Tot_LMS = zeros(1,length(OSNR_dB));
-
 
         % Adding Noise
-        [X_distorted_AWGN, NoiseX] = WGN_Noise_Generation(X_CD, SIG.Sps, M, OSNR_dB(idx_OSNR), SIG.symbolRate);
-        [Y_distorted_AWGN, NoiseY] = WGN_Noise_Generation(Y_CD, SIG.Sps, M, OSNR_dB(idx_OSNR), SIG.symbolRate);
+        [X_distorted_AWGN, NoiseX] = WGN_Noise_Generation(X_CD, SIG.Sps, M, SNR, SIG.symbolRate);
+        [Y_distorted_AWGN, NoiseY] = WGN_Noise_Generation(Y_CD, SIG.Sps, M, SNR, SIG.symbolRate);
 
         % scatterplot(X_distorted_AWGN,8);
         % title('Noisy constellation, before filtering');
@@ -81,26 +82,17 @@ for idx_sweep = 1:length(sweep_par)
             Es = 1; % Symbol energy (=radius)
             Npol = 2;
             windowlen = 100;
-            XY_vit = vit_n_vit(XY_eq, Delta_nu, SIG.symbolRate, OSNR_dB(idx_OSNR), Es, Npol, M, windowlen);
+            XY_vit = vit_n_vit(XY_eq, Delta_nu, SIG.symbolRate, SNR, Es, Npol, M, windowlen);
             X_eq = XY_vit(:,1);
             Y_eq = XY_vit(:,2);
         else
             mu = 1e-3;
             mu2 = 1e-5;
-            % mu = 5e-3;
             NTaps = 9;
             N1 = 5e4;
             N2 = 5e3;
-            if sweep_par(idx_sweep) == 1
-                XY_eq = EQ_func(X_CD_rec,Y_CD_rec,mu,NTaps,"RDE",N1,N2);
-            elseif sweep_par(idx_sweep) == 2
-                mu2 = 5e-4;
-                [X_eq, Y_eq, e_X, e_Y] = EQ_func_N([X_CD_rec,Y_CD_rec],r,mu,mu2,NTaps,N1,N2);
-                XY_eq = [X_eq, Y_eq];
-            else
-                [X_eq, Y_eq, e_X, e_Y] = EQ_func_J([X_CD_rec,Y_CD_rec],r,mu,mu2,NTaps,N1,N2);
-                XY_eq = [X_eq, Y_eq];
-            end
+            [X_eq, Y_eq, e_X, e_Y] = EQ_func_J([X_CD_rec,Y_CD_rec],r,mu,mu2,NTaps,N1,N2);
+            XY_eq = [X_eq, Y_eq];
             carrSynch = comm.CarrierSynchronizer("Modulation", modulation(r), "SamplesPerSymbol", 1,'DampingFactor', 100, 'NormalizedLoopBandwidth',1e-3);
             [X_eq, phEstX] = carrSynch(XY_eq(:,1));
             [Y_eq, phEstY] = carrSynch(XY_eq(:,2));
@@ -168,79 +160,30 @@ for idx_sweep = 1:length(sweep_par)
             end
             k=k+1;
         end
-        BERmat(idx_sweep,idx_OSNR) = .5*(min(X_BER,[],'all')+min(Y_BER,[],'all'));
-        X_Ber_Tot(idx_OSNR) = min(X_BER,[],'all');
-        Y_Ber_Tot(idx_OSNR) = min(Y_BER,[],'all');
+        X_Ber = min(X_BER,[],'all');
+        Y_Ber = min(Y_BER,[],'all');
+        BER(idx_sweep,:) = [BER(idx_sweep,2:end), .5*(X_Ber+Y_Ber)];
+        SNR = SNR+.5;
     end
+    SNR_coarse = SNR-2:.5:SNR-.5;
+    SNR_fine = linspace(SNR-2,SNR-.5,2e4);
+    delta_BER = spline(SNR_coarse,BER(idx_sweep,:),SNR_fine)-BER_goal;
+    [~,min_idx] = min(abs(delta_BER));
+    delta_SNR(idx_sweep) = SNR_fine(min_idx)-SNR_opt;
 end
-save('matrixBER.mat', 'BERmat');
 %%
-% figure;
-% h1 = heatmap(BERmat);
-% h1.Title = 'BER at 8dB';
-% h1.XLabel = 'N';
-% h1.YLabel = 'B';
-% h1.Colormap = parula;  % Ändert das Farbschema
-% h1.XDisplayLabels = Nvec;
-% h1.YDisplayLabels = Bvec;
-% figure;
-% h2 = heatmap(log10(squeeze(BERmat(1,:,:))));
-% h2.Title = 'log10(BER)';
-% h2.YLabel = 'N';
-% h2.XLabel = 'SNR';
-% h2.Colormap = parula;  % Ändert das Farbschema
-% h2.YDisplayLabels = Nvec;
-% h2.XDisplayLabels = OSNR_dB;
-
-%%
-if r == 1
-    BER_TH = 0.5 * erfc(sqrt(10.^(OSNR_dB/10)/2));
-    BER_MED_MF = 0.5 * (X_Ber_Tot + Y_Ber_Tot);
-    figure();
-    semilogy(OSNR_dB, BER_TH, 'r', 'LineWidth', 1);
-    xlim([1,14]);
-    grid on;
-    hold on;
-    semilogy(OSNR_dB, BER_MED_MF, 'Marker','o', 'Color', "#77AC30", 'LineStyle','-.', 'LineWidth', 1);
-    title(sprintf('%s BER curve',MODULATIONS(r)));
-    legend('Theoretical BER','Simulated BER - CMA', 'X Pol', 'Y Pol', 'Interpreter', 'latex');
-    xlabel('OSNR [dB]', 'Interpreter','latex');
-    hold off;
-    fprintf('The BER on Xpol is: %.6f\n', X_Ber_Tot);
-    fprintf('The BER on Ypol is: %.6f\n', Y_Ber_Tot);
-else
-    BER_TH = 3/8 * erfc(sqrt(10.^(OSNR_dB/10)/10));
-    BER_MED_MF = 0.5 * (X_Ber_Tot + Y_Ber_Tot);
-    figure();
-    semilogy(OSNR_dB, BER_TH, 'r', 'LineWidth', 1);
-    % xlim([1,14]);
-    grid on;
-    hold on;
-    semilogy(OSNR_dB, BERmat(1,:), 'Marker','o', 'Color', "#77AC30", 'LineStyle','-.', 'LineWidth', 1);
-    semilogy(OSNR_dB, BERmat(2,:), 'Marker','o', 'LineStyle','-.', 'LineWidth', 1);
-    semilogy(OSNR_dB, BERmat(3,:), 'Marker','o', 'LineStyle','-.', 'LineWidth', 1);
-    % title(sprintf('%s BER curve (B=%i, N=%i)',MODULATIONS(r),Bvec,Nvec));
-    legend('Theoretical BER','Simulated BER - CMA Book', 'Simulated BER - CMA Nicolo', 'Simulated BER - CMA Jonas', 'Interpreter', 'latex');
-    xlabel('OSNR [dB]', 'Interpreter','latex');
-    hold off;
-    fprintf('The BER on Xpol is: %.6f\n', X_Ber_Tot);
-    fprintf('The BER on Ypol is: %.6f\n', Y_Ber_Tot);
 
 
-    % figure();
-    % hold on
-    % for idx_OSNR = 1:length(OSNR_dB)
-    %         SNR = 10*log10(10*erfinv(-8/3.*squeeze(BERmat(:,idx_OSNR))+1).^2);
-    %         plot(log10(sweep_par), OSNR_dB(idx_OSNR)-SNR, 'LineWidth', 1, 'DisplayName', sprintf('OSNR = %d dB', OSNR_dB(idx_OSNR)));
-    % end
-    % % xlim([1,14]);
-    % grid on;
-    % title(sprintf('%s SNR penalty',MODULATIONS(r)));
-    % legend show;
-    % xlabel('$log_{10}\Delta\nu$', 'Interpreter','latex');
-    % ylabel('$\Delta$OSNR [dB]', 'Interpreter','latex');
-    % hold off;
-    % fprintf('The BER on Xpol is: %.6f\n', X_Ber_Tot);
-    % fprintf('The BER on Ypol is: %.6f\n', Y_Ber_Tot);
-end
+
+figure();
+plot(sweep_par, delta_SNR, 'k', 'LineWidth', 2);
+grid on;
+hold on;
+title(sprintf('SNR penalty (%s)',MODULATIONS(r)));
+% legend('Theoretical BER','Simulated BER - CMA', 'X Pol', 'Y Pol', 'Interpreter', 'latex');
+xlabel('Sweep Parameter', 'Interpreter','latex');
+ylabel('$\Delta SNR$', 'Interpreter','latex');
+hold off;
+
+
 runtime_out = toc(runtime)
